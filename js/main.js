97fa2280e83f55d52875ad3089d33e82aeac7bbc
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-04-02 07:33:29 -0400

    Display none on pageload to avoid FOUC

--- js/main.js
+++ js/main.js
@@ -5,42 +5,46 @@ window.onload = function() {
       text    = document.getElementById('text'),
       arrow   = document.getElementById('arrow')
   
-  TweenLite.from(circle, 0.4, {
-    scale: 0
-  });
-  
-  TweenLite.from(cat, 0.5, {
-    scale: 0
-  });
-  
-  TweenLite.from(textBox, 0.5, {
-    width: 0, 
-    delay: 0.2,
-    borderBottom: 0,
-    borderTop: 0,
-    borderRight: 0
-  });
+  setTimeout(function() {
+    TweenLite.from(circle, 0.4, {
+      scale: 0
+    });
+    
+    TweenLite.from(cat, 0.5, {
+      scale: 0
+    });
+    
+    TweenLite.from(textBox, 0.5, {
+      width: 0, 
+      delay: 0.2,
+      borderBottom: 0,
+      borderTop: 0,
+      borderRight: 0
+    });
 
-  TweenLite.from(text, 0.5, {
-    x: -100,
-    scale: 0,
-    delay: 0.2
-  });
+    TweenLite.from(text, 0.5, {
+      x: -100,
+      scale: 0,
+      delay: 0.2
+    });
 
-  TweenLite.from(arrow, 0.4, {
-    scale: 0,
-    top: 290,
-    left: 440,
-    delay: 0.4
-  });
+    TweenLite.from(arrow, 0.4, {
+      scale: 0,
+      top: 290,
+      left: 440,
+      delay: 0.4
+    });
 
-  TweenMax.to(arrow, 0.3, {
-    delay: 1,
-    top: 290,
-    left: 440,
-    repeat: -1,
-    yoyo: true
-  });
+    TweenMax.to(arrow, 0.3, {
+      delay: 1,
+      top: 290,
+      left: 440,
+      repeat: -1,
+      yoyo: true
+    });
+
+    document.getElementById('container').style.display="block";
+  }, 500);
 
   document.getElementById('container').onclick = function() {
     TweenMax.killAll();
